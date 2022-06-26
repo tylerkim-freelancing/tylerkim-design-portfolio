858a16c75aaaa160cdec0537ca80bce0fc440a1f
Hyunbin Tyler Kim <tylerhkmontana@gmail.com>
2022-06-26 03:38:30 -0400

    minor update

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,15 @@ import Head from 'next/head'
 export default function Home({images}) {
   return (
     <>
-    
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="montana design boilerplate" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        
+      </div>
     </>
   )
 }
