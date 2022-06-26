070b6e70b58d9490430d0000ebd3bdcacb6f3860
Hyunbin Tyler Kim <tylerhkmontana@gmail.com>
2022-06-26 03:03:02 -0400

    first commit

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-import styles from './styles/footer.module.scss'
-
-export default function Footer() {
-  return (
-    <div className={styles.footer}>
-      <p>&copy; 2021 by Magi Web Production. All rights reserved.</p>
-    </div>
-  );
-}
