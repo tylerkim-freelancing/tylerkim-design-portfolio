409e5fb177f43f62b34bc8d6abf86363ad456656
Hyunbin Tyler Kim <tylerhkmontana@gmail.com>
2022-07-10 14:20:19 -0400

    update index.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="description" content="montana design boilerplate" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
 import styles from '../styles/pages/home.module.scss'
-import Head from 'next/head'
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="montana design boilerplate" />
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className={styles.main}>
-        
-      </div>
+      <Layout title='Home'>
+        <div className={styles.main}>
+
+        </div>
+      </Layout>
     </>
   )
 }
