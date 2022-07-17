3894018ae4a1854b3a71a1187816426df1250f5c
Hyunbin Tyler Kim <tylerhkmontana@gmail.com>
2022-07-17 16:10:13 -0400

    card component added

---
+++ components/card.js
@@ -0,0 +1,11 @@
+import styles from './styles/card.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Card({ url }) {
+    return (
+        <div className={styles.card}>
+
+        </div>
+    )
+}
