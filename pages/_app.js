34bb7c76b99dab97a4dcff39f1cb4ed0e5db9697
Hyunbin Tyler Kim <tylerhkmontana@gmail.com>
2022-08-06 13:46:46 -0400

    og tags added

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,9 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <meta name="description" content="montana design boilerplate" />
+        <meta property="og:title" content="Tyler Kim | The Best Web Developer" />
+        <meta property="og:description" content="I make the best personal websites that can grow in to a business" />
+        <meta property="og:image" content="/images/projile.jpg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
