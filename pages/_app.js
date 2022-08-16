c550bdfdb6890456969b8aed03bb64711bd83ea3
Hyunbin Tyler Kim <tylerhkmontana@gmail.com>
2022-08-16 03:04:07 -0400

    thumbnails updated

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <meta name="description" content="montana design boilerplate" />
+        <meta name="description" content="Tyler Kim's Portfolio Website" />
         <meta property="og:title" content="Tyler Kim | The Best Web Developer" />
         <meta property="og:description" content="I make the best personal websites that can grow in to a business" />
         <meta property="og:image" content="https://tylerkim-design-portfolio.vercel.app/api/images?title=profile.jpg" />
